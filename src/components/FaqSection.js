973781f58500f2145b564c2d58154f02fd8ba50d
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2020-11-23 16:47:11 +0100

    Ading the FAQ section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions? <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I start ?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
+            doloremque.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedul</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
+            doloremque.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different payment methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
+            doloremque.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Products do you offer ?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
+            doloremque.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,13 +2,14 @@ import React from "react";
 //Page Components
 import AboutSection from "../components/AboutSection";
 import ServiceSection from "../components/ServicesSection";
-import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServiceSection />
+      <FaqSection />
     </>
   );
 };
