e83da0d2782341ab96df42c3e6cea5e9574343cd
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2020-11-26 12:09:41 +0100

    Adding animations to Our Work section

--- src/animation.js
+++ src/animation.js
@@ -7,16 +7,16 @@ export const pageAnimation = {
     opacity: 1,
     y: 0,
     transition: {
-      duration: 1,
+      duration: 0.5,
       when: "beforeChildren",
       staggerChildren: 0.25,
     },
   },
   exit: {
     opacity: 0,
-    y: 300,
+    y: 50,
     transition: {
-      duration: 0.5,
+      duration: 0.3,
     },
   },
 };
@@ -47,4 +47,29 @@ export const photoAnim = {
       duration: 0.75,
     },
   },
+};
+
+export const lineAnim = {
+  hidden: { width: "0%" },
+  show: {
+    width: "100%",
+    transition: { duration: 0.75 },
+  },
+};
+
+export const slider = {
+  hidden: { x: "-130%", skew: "45deg" },
+  show: {
+    x: "100%",
+    skew: "0deg",
+    transition: {
+      ease: "easeOut",
+      duration: 1,
+    },
+  },
+};
+
+export const sliderContainer = {
+  hidden: { opacity: 1 },
+  show: { opacity: 1, transition: { staggerChildren: 0.15, ease: "easeOut" } },
 };
--- src/components/Wave.js
+++ src/components/Wave.js
@@ -1,4 +1,4 @@
-import Recat from "react";
+import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
